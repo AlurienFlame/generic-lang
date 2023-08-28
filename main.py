7aa42da84beee38996cc3d6ecb97ff8410fde087
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2023-08-29 08:56:01 +1200

    Read code in from file instead of stdin

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from lark import Lark
 from transformer import MyTransformer
+from argparse import ArgumentParser
+
 
 def main():
     # Load grammar from file
     grammar = None
-    with open('grammar.ebnf') as f:
+    with open("grammar.ebnf") as f:
         grammar = f.read()
 
     # Create parser and transformer
-    parser = Lark(grammar, start='start', parser='lalr')
+    parser = Lark(grammar, start="start", parser="lalr")
     transformer = MyTransformer()
 
-    # Main loop
-    while True:
-        expression = input("> ")
-        syntax_tree = parser.parse(expression)
-        print(syntax_tree.pretty())
-        result = transformer.transform(syntax_tree)
-        print(transformer.symbol_table)
-        print(result)
+    # Load source code from file
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("source_file", help="The source file to compile")
+    args = arg_parser.parse_args()
+
+    source_code = None
+    with open(args.source_file) as f:
+        source_code = f.read()
+
+    # Parse and run the source code
+    syntax_tree = parser.parse(source_code)
+    print(syntax_tree.pretty())
+    result = transformer.transform(syntax_tree)
+    print(transformer.symbol_table)
+    print(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
